ce40621e107a36df2061a502895dcde5398274a9
Nicholas Saunders <nksaunders1994@gmail.com>
2019-05-23 10:37:50 -0700

    big fixes

--- happyplanets/lightcurve.py
+++ happyplanets/lightcurve.py
@@ -29,36 +29,56 @@ from .correct import PyMC_PLD
 
 class LightCurve(object):
 
-    def __init__(self):
+    def __init__(self, target_name, system):
         ''' '''
-        pass
+        self.target_name = target_name
+        self.system = system
+        self.global_aperture = 'pipeline'
 
 
-    def download_files(target_name):
+    def return_lightcurve(self):
+        ''' '''
+
+        tpf_collection = self.download_files(self.target_name)
+        return self.detrend(tpf_collection)
+
+
+    def detrend(self, tpf_collection):
+        ''' '''
+
+        # assign variables
+        x = np.array([], np.float64)
+        y = np.array([], np.float64)
+        yerr = np.array([], np.float64)
+
+        for tpf in tpf_collection:
+            mask = self.system.create_planet_mask(tpf.time)
+
+            # make sure the pipeline aperture has at least 3 pixels
+            if np.sum(tpf._parse_aperture_mask('pipeline'), axis=(0,1)) >= 3:
+                aperture_mask = tpf._parse_aperture_mask(self.global_aperture)
+            else:
+                aperture_mask = tpf._parse_aperture_mask('threshold')
+            time, flux, error, gp = PyMC_PLD(tpf, self.system, mask, aperture_mask)
+
+            x = np.append(x, time)
+            y = np.append(y, flux)
+            yerr = np.append(yerr, error)
+
+        lc = lk.LightCurve(time=x, flux=y, flux_err=yerr)
+
+        '''
+        Make gp_lc a lk.LightCurve object and set it as a self variable.
+        gp_lc = lk.LightCurve(time=x, flux=gp)
+        '''
+
+        return lc
+
+
+    def download_files(self, target_name):
         ''' '''
 
         search_result = lk.search_targetpixelfile(target_name)
         tpf_collection = search_result.download_all(quality_bitmask='hardest')
 
         return tpf_collection
-
-
-    def detrend(self, tpf_collection):
-        ''' '''
-
-        for tpf in tpf_collection:
-            mask = np.zeros_like(tpf.time, dtype=bool)
-            for i in range(self.n_planets):
-                mask |= self.get_transit_mask(tpf.time, self.pl_t0[i], self.pl_period[i], 0.3)
-            # make sure the pipeline aperture has at least 3 pixels
-            if np.sum(tpf._parse_aperture_mask('pipeline'), axis=(0,1)) >= 3:
-                aperture_mask = tpf._parse_aperture_mask(self.global_aperture)
-            else:
-                aperture_mask = tpf._parse_aperture_mask('threshold')
-            time, flux, error, gp = self.PyMC_PLD(tpf, mask, aperture_mask)
-
-            x = np.append(x, time)
-            y = np.append(y, flux)
-            yerr = np.append(yerr, error)
-
-        return x, y, yerr, gp
