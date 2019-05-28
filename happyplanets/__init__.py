556196ec59b2f57f6bfcd517b025a7937ab4790c
Nicholas Saunders <nksaunders1994@gmail.com>
2019-05-28 15:25:46 -0700

    imports

--- happyplanets/__init__.py
+++ happyplanets/__init__.py
@@ -2,5 +2,7 @@ import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 from .correct import *
-from .system import *
+from .timeseries import *
+from .planetsystem import *
 from .transitfit import *
+from .utils import *
