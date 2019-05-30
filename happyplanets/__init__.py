d970e838d21b63456fc2cfe56a65a30bc2a44e48
Nicholas Saunders <nksaunders1994@gmail.com>
2019-05-30 16:16:50 -0700

    remove timeseries

--- happyplanets/__init__.py
+++ happyplanets/__init__.py
@@ -2,7 +2,6 @@ import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 from .correct import *
-from .timeseries import *
 from .planetsystem import *
 from .transitfit import *
 from .utils import *
